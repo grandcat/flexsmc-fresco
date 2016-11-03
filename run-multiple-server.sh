55c56f8492cf99898f5971eb7f6d902f0f99890a
Stefan Smarzly <smarzlys@gmail.com>
2016-11-03 11:49:36 +0100

    chore: remove comments

--- run-multiple-server.sh
+++ run-multiple-server.sh
@@ -5,6 +5,4 @@ until [  $COUNTER -lt 1 ]; do
 	java -cp target/flexsmc-fresco-0.0.1-SNAPSHOT-jar-with-dependencies.jar de.tum.flexsmc.smc.CLIMain -c "unix:///tmp/grpc${COUNTER}.sock" &
 	let COUNTER-=1
 done
-#java -cp target/flexsmc-fresco-0.0.1-SNAPSHOT-jar-with-dependencies.jar de.tum.flexsmc.smc.CLIMain -c "unix:///tmp/grpc1.sock" &
-#java -cp target/flexsmc-fresco-0.0.1-SNAPSHOT-jar-with-dependencies.jar de.tum.flexsmc.smc.CLIMain -c "unix:///tmp/grpc2.sock" &
-#java -cp target/flexsmc-fresco-0.0.1-SNAPSHOT-jar-with-dependencies.jar de.tum.flexsmc.smc.CLIMain -c "unix:///tmp/grpc3.sock" &
+
