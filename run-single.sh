2a8a9c1f0d8c52c9c5bb0dbbafc6f86eb548e041
Stefan Smarzly <smarzlys@gmail.com>
2017-01-02 16:03:23 +0100

    refact(scripts): remove unnecessary script

--- run-single.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-java -Djava.util.logging.config.file="logging.properties" -cp target/flexsmc-fresco-0.0.1-SNAPSHOT-jar-with-dependencies.jar de.tum.flexsmc.smc.CLIMain -c "unix:///tmp/grpc-flexsmc.sock"
-
