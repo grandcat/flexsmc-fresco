2cedc09b317fad7b2ec7425077c3b0137eccf513
Stefan Smarzly <smarzlys@gmail.com>
2017-01-02 15:58:15 +0100

    refact(scripts): use single parameterized script to start Fresco instances

--- run-multiple-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-COUNTER=3
-until [  $COUNTER -lt 1 ]; do
-	echo COUNTER $COUNTER
-	java -Djava.util.logging.config.file="logging.properties" -cp target/flexsmc-fresco-0.0.1-SNAPSHOT-jar-with-dependencies.jar de.tum.flexsmc.smc.CLIMain -c "unix:///tmp/grpc${COUNTER}.sock" &
-	let COUNTER-=1
-done
-
---
+++ start-server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Cleanup any running left-over.
+ps aux | grep "[j]ava -Djava.util.logging.config.file=logging.properties -cp target/flexsmc-fresco" | awk '{print $2}' | xargs kill
+# Start instances in background with logging to /tmp (overwrites previous logs).
+fresco_instances=${1:-1}
+until [  $fresco_instances -lt 1 ]; do
+	echo "Starting instance ${fresco_instances}"
+	nohup java -Djava.util.logging.config.file="logging.properties" -cp target/flexsmc-fresco-0.0.1-SNAPSHOT-jar-with-dependencies.jar de.tum.flexsmc.smc.CLIMain -c "unix:///tmp/grpc-flexsmc${fresco_instances}.sock" 2>&1> /tmp/flexsmc-fresco-s${fresco_instances}.log &
+	let fresco_instances-=1
+done
